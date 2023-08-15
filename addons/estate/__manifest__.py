28396782bc5f3ee5a91194ce0e9f0529c4fc4668
Andres Bayona <andres.bayona@outlook.com>
2023-08-14 22:38:23 -0500

    Unnecessary data in the manifest.py file is removed

--- addons/estate/__manifest__.py
+++ addons/estate/__manifest__.py
@@ -1,16 +1,4 @@
 {
     'name': "Real Estate",
-    #'version': '1.0',
-    'depends': ['base'],
-    #'author': "Author Name",
-    #'category': 'Category',
-    #'description': "",
-    # data files always loaded at installation
-    #'data': [
-    #    'views/mymodule_view.xml',
-    #],
-    # data files containing optionally loaded demonstration data
-    #'demo': [
-    #    'demo/demo_data.xml',
-    #],
+    'depends': ['base']
 }
