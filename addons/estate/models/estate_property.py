cab85e37a9e558ffc4973418b67ddeaefe35b060
Andres Bayona <andres.bayona@outlook.com>
2023-08-15 19:03:07 -0500

    Correction of the garden_orientation field, and addition of attributes to the fields..

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -8,12 +8,12 @@ class EstateProperty(models.Model):
     _name = "estate.property"
     _description = "Real estate services"
     #_order = "sequence"
-
-    name = fields.Char()
+    
+    name = fields.Char(required=True)
     description = fields.Text()
     postcode = fields.Char()
     date_availability = fields.Date()
-    expected_price = fields.Float()
+    expected_price = fields.Float(required=True)
     selling_price = fields.Float()
     bedrooms = fields.Integer()
     living_area = fields.Integer()
@@ -23,9 +23,8 @@ class EstateProperty(models.Model):
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
         string='Type',
-        selection=[('North'), ('South'), ('East'), ('West')]
+        selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
     )
-
 
 
     """
