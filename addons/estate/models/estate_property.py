7ade73d30e9a5e4d97db71b6f26b64749ef06a16
Andres Bayona <andres.bayona@outlook.com>
2023-08-17 20:18:39 -0500

    End of chapter 7. Custom search creation and some filters implementing logical operators.

--- addons/estate/models/estate_property.py
+++ addons/estate/models/estate_property.py
@@ -25,7 +25,7 @@ class EstateProperty(models.Model):
     garden = fields.Boolean()
     garden_area = fields.Integer()
     garden_orientation = fields.Selection(
-        string='Type',
+        string='Garden Orientation',
         selection=[('north', 'North'), ('south', 'South'), ('east', 'East'), ('west', 'West')]
     )
     active = fields.Boolean(default=True)
